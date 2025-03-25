83607fb68def7ec0a72da7a6d433461c9cf514ed
Anass El khamlichi <anasselkhamlichi0000@gmail.com>
2025-03-25 02:26:08 +0000

    add 3 section courses with resposive

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
-import Navbar from './components/navbar/Navbar'
 import Hero from './components/hero/Hero'
 import Brand from './components/brand/Brand'
 import Courses from './components/courses/Courses'
 
+
 const App = () => {
   return (
-    <main className='overflow-x-hidden text-white bg-black'> 
+    <main className='overflow-x-hidden text-white bg-[#242424]'> 
      
        <Hero />
       <Brand />
-      <Courses />  
+      <Courses />
+      
     </main>
   )
 }
--- src/components/courses/Courses.jsx
+++ src/components/courses/Courses.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
+import CoursesCard from './CoursesCard'
+import CoursesCard2 from './CoursesCard2'
+import CoursesCard3 from './CoursesCard3'
 
 const Courses = () => {
   return (
-    <section>
-       <div className="container">
-           {/* Title Header   */}
-           <p>Courses</p>
-           <h1 className=''>Find the Perfect German Course for You!</h1>
-       </div>
-     </section>
+    <section className='py-12 md:py-24'>
+        <div className="container">
+            {/* Title  */}
+            <div className='text-center'>
+            <p className='text-sm'>Courses</p>
+            <h1 className='text-3xl md:text-4xl font-bold'>Find the Perfect German Course for You!</h1>
+            </div>
+            {/* CoursesCard stard */}
+            <CoursesCard />
+            <CoursesCard2 />
+           <CoursesCard3 />
+        </div>
+    </section>
   )
 }
 
